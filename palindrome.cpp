ba0d037eac33dd2fd48aa1d74407d374f3f85bc8
Saranda Poddar <55356439+saranda-2811@users.noreply.github.com>
2022-04-01 10:56:27 +0530

    Update and rename palidrome.cpp to palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,12 +4,16 @@ using namespace std;
 bool palindrome(string s, int start, int end) {
     if(start == end)
         return true;
+	
     if(s[start]!=s[end])
         return false;
+	
     if(start<end+1)
         return palindrome(s, start+1, end-1);
+	
     return true;
 }
+
 int main() {
 	string s;
 	cin>>s;
@@ -18,6 +22,7 @@ int main() {
 	     cout<<"is palindrome";  
 	     return 0;
 	 }
+	
 	bool ans = palindrome(s,0,s.length()-1);
 	if(ans==1)
 	    cout<<"is palindrome";
